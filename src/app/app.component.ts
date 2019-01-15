b1ca8157e0ee9af81cafef11613a1991ecb751ee
H338091 <ravikant.kumar@honeywell.com>
2019-01-15 12:12:48 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 0.00;
+  title ='First Page';
+  price = 9.10;
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,69 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatChipsModule, MatOptionModule, MatGridListModule, MatProgressBarModule, MatSliderModule, MatSlideToggleModule, MatMenuModule, MatDialogModule, MatSnackBarModule, MatSelectModule, MatInputModule, MatSidenavModule, MatCardModule, MatIconModule, MatRadioModule, MatProgressSpinnerModule, MatTabsModule, MatListModule } from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyNavComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatButtonModule, 
+    MatCheckboxModule, 
+    MatToolbarModule, 
+    MatChipsModule,
+    MatOptionModule, 
+    MatGridListModule, 
+    MatProgressBarModule, 
+    MatSliderModule, 
+    MatSlideToggleModule, 
+    MatMenuModule, 
+    MatDialogModule, 
+    MatSnackBarModule, 
+    MatSelectModule, 
+    MatInputModule, 
+    MatSidenavModule, 
+    MatCardModule, 
+    MatIconModule, 
+    MatRadioModule, 
+    MatProgressSpinnerModule, 
+    MatTabsModule,
+    MatListModule
+  ],
+  exports: [BrowserAnimationsModule,
+    LayoutModule,
+    MatButtonModule, 
+    MatCheckboxModule, 
+    MatToolbarModule, 
+    MatChipsModule,
+    MatOptionModule, 
+    MatGridListModule, 
+    MatProgressBarModule, 
+    MatSliderModule, 
+    MatSlideToggleModule, 
+    MatMenuModule, 
+    MatDialogModule, 
+    MatSnackBarModule, 
+    MatSelectModule, 
+    MatInputModule, 
+    MatSidenavModule, 
+    MatCardModule, 
+    MatIconModule, 
+    MatRadioModule, 
+    MatProgressSpinnerModule, 
+    MatTabsModule,
+    MatListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
